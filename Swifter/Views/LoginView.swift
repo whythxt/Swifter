56624598ed3fa97273c5a2661a772b07b06d9164
Yurii <yuraznakharchuk@gmail.com>
2022-08-04 00:11:18 +0300

    Sign up & Log in views

---
+++ Swifter/Views/LoginView.swift
@@ -0,0 +1,83 @@
+//
+//  LoginView.swift
+//  Swifter
+//
+//  Created by Yurii on 03.08.2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var email = ""
+    @State private var password = ""
+    
+    var disabled: Bool {
+        email.trimmingCharacters(in: .whitespaces).isEmpty || password.trimmingCharacters(in: .whitespaces).isEmpty ? true : false
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Button("Cancel") {
+                    dismiss()
+                }
+                .foregroundColor(.black)
+                
+                Spacer()
+                
+                Image(systemName: "swift")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(.orange)
+                
+                Spacer()
+                
+                Button("Cancel") { }.opacity(0.0)
+                
+            }
+            .padding(.horizontal, 5)
+            
+            Spacer()
+            
+            VStack(spacing: 20) {
+                TextField("Email", text: $email)
+                    .keyboardType(.emailAddress)
+                
+                Divider()
+                
+                SecureField("Password", text: $password)
+                Divider()
+            }
+            .padding(.horizontal, 30)
+            
+            Button {
+                
+            } label: {
+                Text("Log in")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 350, height: 50)
+                    .background(disabled ? .gray : .black)
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .disabled(disabled)
+        
+            Spacer()
+        }
+    }
+    
+    func clearInput() {
+        email = ""
+        password = ""
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ Swifter/Views/SignUpView.swift
@@ -0,0 +1,91 @@
+//
+//  SignUpView.swift
+//  Swifter
+//
+//  Created by Yurii on 03.08.2022.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @State private var name = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var username = ""
+    
+    @State private var showingLogin = false
+    
+    var disabled: Bool {
+        name.trimmingCharacters(in: .whitespaces).isEmpty || email.trimmingCharacters(in: .whitespaces).isEmpty || password.trimmingCharacters(in: .whitespaces).isEmpty || username.trimmingCharacters(in: .whitespaces).isEmpty ? true : false
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 25, height: 25)
+                .foregroundColor(.orange)
+            
+            Spacer()
+            
+            VStack(spacing: 20) {
+                TextField("Name", text: $name)
+                Divider()
+                
+                TextField("Email", text: $email)
+                    .keyboardType(.emailAddress)
+                
+                Divider()
+                
+                SecureField("Password", text: $password)
+                Divider()
+                
+                TextField("Username", text: $username)
+                Divider()
+            }
+            .padding(.horizontal, 30)
+            
+            Button {
+                clearInput()
+            } label: {
+                Text("Create account")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 350, height: 50)
+                    .background(disabled ? .gray : .black)
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .disabled(disabled)
+            
+            Spacer()
+            
+            HStack {
+                Text("Have an account already?")
+                    .foregroundColor(.secondary)
+                
+                Button("Log in") {
+                    showingLogin.toggle()
+                }
+            }
+            .font(.subheadline)
+        }
+        .fullScreenCover(isPresented: $showingLogin) {
+            LoginView()
+        }
+    }
+    
+    func clearInput() {
+        name = ""
+        email = ""
+        password = ""
+        username = ""
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
